df339f82183f8b7451fc66a880a9005babcb1e59
Simplify-ERP® <github@euroblaze.de>
2023-04-27 23:35:22 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import PasswordField, BooleanField
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -22,4 +23,14 @@ class SourceForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     city = StringField('City')
     postal_code = StringField('Postal Code')
+    submit = SubmitField('Submit')
+
+class AdminLoginForm(FlaskForm):
+    master_password = PasswordField('Master Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    is_admin = BooleanField('Is Admin')
     submit = SubmitField('Submit')
