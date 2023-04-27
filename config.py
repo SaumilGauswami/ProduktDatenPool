d2aecde1eafe6ccb4338d9050cdbf62f16a0ae13
Simplify-ERP® <github@euroblaze.de>
2023-04-27 23:28:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,8 @@ SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/database_name'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
 ALLOWED_EXTENSIONS = {'csv'}
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
