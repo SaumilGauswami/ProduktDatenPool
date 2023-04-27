53ff6b428b1d1ac071c2dd11a83b22d47600f53d
Simplify-ERP® <github@euroblaze.de>
2023-04-27 23:25:16 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from app import db, login_manager
+from flask_sqlalchemy import SQLAlchemy
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,3 +11,13 @@ class User(db.Model, UserMixin):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+db = SQLAlchemy()
+
+class Source(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=True)
+    postal_code = db.Column(db.String(10), nullable=True)
+    last_updated = db.Column(db.DateTime, nullable=True)
