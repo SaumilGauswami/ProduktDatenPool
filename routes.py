85fbd3263119b5e231f7fcb7b9e38d30ef36cb9e
Simplify-ERP® <github@euroblaze.de>
2023-04-27 23:43:34 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -56,3 +56,56 @@ def users():
 def logout():
     session.pop('admin_logged_in', None)
     return redirect(url_for('admin_login'))
+
+# Add this import to the existing routes.py
+from flask import abort
+
+# Update the existing sources route
+@app.route('/sources', methods=['GET', 'POST'])
+def sources():
+    if 'admin_logged_in' not in session or not session['admin_logged_in']:
+        return redirect(url_for('admin_login'))
+
+    form = SourceForm()
+    if form.validate_on_submit():
+        source = Source(url=form.url.data, name=form.name.data, city=form.city.data, postal_code=form.postal_code.data)
+        db.session.add(source)
+        db.session.commit()
+        flash('Source added successfully.')
+        return redirect(url_for('sources'))
+
+    sources = Source.query.all()
+    return render_template('sources.html', sources=sources, form=form)
+
+# Add this new route for editing sources
+@app.route('/sources/edit/<int:source_id>', methods=['GET', 'POST'])
+def edit_source(source_id):
+    if 'admin_logged_in' not in session or not session['admin_logged_in']:
+        return redirect(url_for('admin_login'))
+
+    source = Source.query.get_or_404(source_id)
+    form = SourceForm(obj=source)
+
+    if form.validate_on_submit():
+        source.url = form.url.data
+        source.name = form.name.data
+        source.city = form.city.data
+        source.postal_code = form.postal_code.data
+        db.session.commit()
+        flash('Source updated successfully.')
+        return redirect(url_for('sources'))
+
+    return render_template('edit_source.html', form=form, source=source)
+
+# Add this new route for deactivating sources
+@app.route('/sources/deactivate/<int:source_id>', methods=['POST'])
+def deactivate_source(source_id):
+    if 'admin_logged_in' not in session or not session['admin_logged_in']:
+        abort(403)
+
+    source = Source.query.get_or_404(source_id)
+    source.active = not source.active
+    db.session.commit()
+
+    return redirect(url_for('sources'))
+
