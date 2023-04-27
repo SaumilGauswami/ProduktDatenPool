9f47004778214beb7566c08c7e97d19bad584e90
Simplify-ERP® <github@euroblaze.de>
2023-04-27 23:36:30 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import SourceForm
 from app.models import Source
+from flask import session, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/sources', methods=['GET', 'POST'])
 def sources():
@@ -15,3 +17,42 @@ def sources():
 
     sources = Source.query.all()
     return render_template('sources.html', sources=sources, form=form)
+
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    if 'admin_logged_in' in session and session['admin_logged_in']:
+        return redirect(url_for('users'))
+
+    form = AdminLoginForm()
+    if form.validate_on_submit():
+        master_password = app.config.get('MASTER_PASSWORD')
+        if check_password_hash(generate_password_hash(master_password), form.master_password.data):
+            session['admin_logged_in'] = True
+            return redirect(url_for('users'))
+        else:
+            flash('Invalid master password.')
+
+    return render_template('admin_login.html', form=form)
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if 'admin_logged_in' not in session or not session['admin_logged_in']:
+        return redirect(url_for('admin_login'))
+
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        user.is_admin = form.is_admin.data
+        db.session.add(user)
+        db.session.commit()
+        flash('User added successfully.')
+        return redirect(url_for('users'))
+
+    users = User.query.all()
+    return render_template('users.html', users=users, form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('admin_logged_in', None)
+    return redirect(url_for('admin_login'))
