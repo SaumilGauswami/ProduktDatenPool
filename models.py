8f048d93c73582f96d134a6214358cdd49a76e54
Simplify-ERP® <github@euroblaze.de>
2023-04-27 23:34:09 +0200

    Update models.py

--- models.py
+++ models.py
@@ -21,3 +21,17 @@ class Source(db.Model):
     city = db.Column(db.String(255), nullable=True)
     postal_code = db.Column(db.String(10), nullable=True)
     last_updated = db.Column(db.DateTime, nullable=True)
+
+# Add this to the existing models.py
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
